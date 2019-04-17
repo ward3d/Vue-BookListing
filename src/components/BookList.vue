0e5264d43e2f910357a5b60485e6b88d9914fc3e
Edward Anderson <edward.anderson@mab.org.uk>
2019-04-17 15:32:32 +0100

    Creating a Child Component

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,38 +1,34 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
-    <ul>
-      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
-    </ul>
-  </div>
+	<div>
+		<h1>{{title}}</h1>
+		<ul>
+			<li v-for="book in books">{{book.title}}:{{book.author}}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-export default {
-  name:'BookList',
-  data() {
-    return {
-      title: 'All Books',
-      books: [
-        {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-        {title: 'American Gods', author: 'Neil Gaiman'},
-        {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-      ]
-    }
-  }
-}
+	export default {
+		name:'BookList',
+		data() {
+			return {
+				title: 'All Books',
+				books: [
+				{title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+				{title: 'American Gods', author: 'Neil Gaiman'},
+				{title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+				]
+			}
+		}
+	}
 </script>
 
 <style>
-  h1, h2 {
-    font-weight: normal;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: block;
-    margin: 0 10px;
-  }
+	h1, h2 {
+		font-weight: normal;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
 </style>
